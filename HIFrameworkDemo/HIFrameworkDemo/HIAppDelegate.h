7efbc224efa4e054ff783f24c15056789e690891
li.zhenjie <swplzj@gmail.com>
2015-01-11 17:29:00 +0800

    提交项目工程

---
+++ HIFrameworkDemo/HIFrameworkDemo/HIAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  HIAppDelegate.h
+//  HIFrameworkDemo
+//
+//  Created by li.zhenjie on 15-1-11.
+//  Copyright (c) 2015年 swplzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ HIFrameworkDemo/HIFrameworkDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HIFrameworkDemo
+//
+//  Created by li.zhenjie on 15-1-11.
+//  Copyright (c) 2015年 swplzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HIAppDelegate class]));
+    }
+}
